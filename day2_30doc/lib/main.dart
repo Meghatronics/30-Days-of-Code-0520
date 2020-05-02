d99a07316b46f3ac9222355613bf4b30c07b81e5
MajorE <dolea_exe@outlook.com>
2020-05-02 14:22:49 +0100

    day 2 start project from day 1

---
+++ day2_30doc/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:day1_30doc/MovieListBody.dart';
+
+void main() => runApp(MovieListing());
+
+class MovieListing extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+   
+        title: 'Movie Listing',
+        home: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              leading: Icon(Icons.local_movies),
+              backgroundColor: Color.fromARGB(120, 240, 50, 100),
+              title: Text('Top 2020 Movies'),
+            ),
+            body: MovieListBody(),
+          ),
+        ));
+  }
+}
+
